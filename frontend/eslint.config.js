49d1550ea91e9bd7502ba0ed03ef92642cad39d5
Vyacheslav Lavrikov <lavrikov2398@gmail.com>
2025-04-26 21:36:31 +0300

    chore(frontend): add eslint-plugin-react

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,27 +2,29 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reacteslint from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.recommended,
+      reacteslint.configs.flat.recommended,
+      reacteslint.configs.flat["jsx-runtime"],
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   }
 );
