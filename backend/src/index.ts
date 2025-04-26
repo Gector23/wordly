33f59a79427127226847eeba4a6a88bd7d2bef5d
Vyacheslav Lavrikov <lavrikov2398@gmail.com>
2025-04-26 19:30:31 +0300

    chore: add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 
 import { startMongoDB } from "./db/mongodb";
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api", routes);
 
 startMongoDB();
